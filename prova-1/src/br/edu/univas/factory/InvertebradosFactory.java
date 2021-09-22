115989822e9badce51525fddd9cb38e6e6d9d663
Livia <carolinabak2@gmail.com>
2021-09-21 22:01:37 -0300

    Create InvertebradosFactory.java

---
+++ prova-1/src/br/edu/univas/factory/InvertebradosFactory.java
@@ -0,0 +1,14 @@
+package br.edu.univas.factory;
+
+import br.edu.univas.vo.Animaleat;
+import br.edu.univas.vo.Shark;
+
+public class InvertebradosFactory implements AnimalsFactory{
+
+	@Override
+	public Animaleat createAnimal() {
+		
+		return null;
+	}
+
+}
